52f8a3b0cfd6100680c17e46f0cd266f14986dfa
noelleter <shdmsrla@gmail.com>
2019-11-11 09:20:30 +0900

    template

--- OOD10/swap_template.cpp
+++ OOD10/swap_template.cpp
@@ -1,42 +1,40 @@
 //
-//  main.cpp
+//  swap_template.cpp
 //  OOD10
 //
 //  Created by 김노은 on 11/11/2019.
 //  Copyright © 2019 Noeun-Kim. All rights reserved.
 //
 
+#include <stdio.h>
 #include <iostream>
 
 /* template 을 사용한 swap 함수 구현(실습 1) */
 /* template 을 사용한 swap 함수에 string type specialization */
-/*
- 
- void swapInt(int& a, int& b){
-    int temp = a;
+
+template<typename T>
+
+void swap(T& a, T& b){
+    T temp = a;
     a = b;
     b = temp;
 }
-
-
-void swapFloat(float& a, float& b){
-    float temp = a;
-    a = b;
-    b = temp;
-}
-
 int main(){
     int a = 1;
     int b = 2;
     float c = 3.3;
     float d = 4.4;
     
-    swapInt(a, b);
-    swapFloat(c, d);
+    std::string e = "World";
+    std::string f = "Hello";
+    
+    swap<int>(a, b);
+    swap<float>(c, d);
+    // swap<std::string>(e, f);
     
     std::cout << "a: " << a << ", b: " << b << std::endl;
     std::cout << "c: " << c << ", d: " << d << std::endl;
-    
+    std::cout << "e: " << c << ", f: " << d << std::endl;
     return 0;
 }
-*/
+
